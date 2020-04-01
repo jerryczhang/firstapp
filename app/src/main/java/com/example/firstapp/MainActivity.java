21f090bccb8891ac66baaa62a0316237c174b91a
jerryczhang <jerrychzhang@gmail.com>
2020-04-01 14:40:56 -0700

    Add animation to main activity

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -3,17 +3,25 @@ package com.example.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    AnimationDrawable wolfAnimation;
+    boolean animationOn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView wolfImage = (ImageView) findViewById(R.id.wolf_anim);
+        wolfImage.setBackgroundResource(R.drawable.wolf);
+        wolfAnimation = (AnimationDrawable) wolfImage.getBackground();
     }
 
     public void sendMessage(View view) {
@@ -22,5 +30,15 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    public void animateWolf(View view) {
+        animationOn = !animationOn;
+        if (animationOn) {
+            wolfAnimation.start();
+        }
+        else {
+            wolfAnimation.stop();
+        }
     }
 }
